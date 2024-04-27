7a8027eb8bb659baf86a29e2cf23a61d925ea6e9
iamalreadynoob <sadikefe69@gmail.com>
2024-04-28 02:21:11 +0300

    version updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.9'
+VERSION = '0.2.0'
 DESCRIPTION = 'Data Science Library'
 LONG_DESCRIPTION = 'A package for data science'
 
@@ -20,7 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['scikit-learn', 'pandas', 'numpy', 'hyperopt', 'catboost', 'imblearn'],
+    install_requires=['scikit-learn', 'pandas', 'numpy', 'hyperopt', 'catboost', 'imblearn', 'lightgbm'],
     keywords=['python', 'machine', 'learning', 'machine learning', 'data science', 'data'],
     py_modules=['data_tools', 'model_tools', 'progressive_tools', 'feature_tools'],
     classifiers=[
